ef9720bc52ae3b9b7569731c386e2e79c25462f6
bijoy133 <bijoydas5680@!gmail.com>
2025-06-11 22:09:46 +0600

    get single package

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app=express();
 const port =process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware 
@@ -43,6 +43,13 @@ async function run() {
     app.get('/packages', async(req,res)=>{
       const cursor = packagesCollection.find();
       const result = await cursor.toArray()
+      res.send(result)
+    })
+    // package details api
+    app.get('/packages/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result =await packagesCollection.findOne(query);
       res.send(result)
     })
 
